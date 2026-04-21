a43890e4947b76ec1d8f2c238713780d8aebd4d0
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2026-04-21 10:00:03 +0530

    change

--- React/demo_app/src/App.js
+++ React/demo_app/src/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Func_component from './component/Func_component'
 import Class_component from './component/Class_component'
 
+import Jsx_comp from './JSX/Jsx_comp'
+import Layout from './Layout/Layout'
+
 function App() {
   return (
 	<div>
-		<Func_component/>
-			<h1>Main content</h1>
-		<Class_component/>
+		{
+			//<Func_component/>
+			//<Class_component/>
+			//<Jsx_comp/>
+
+		}
+		
+		<Layout/>
 	</div>
   )
 }
