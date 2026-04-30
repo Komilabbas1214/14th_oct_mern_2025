e6826a3c92d7dfe84a77960a386fc78345e9f7b1
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2026-04-30 09:57:23 +0530

    change

--- React/demo_app/src/App.js
+++ React/demo_app/src/App.js
@@ -7,20 +7,26 @@ import Layout from './Layout/Layout'
 import Css_apply from './css_apply/Css_apply'
 import Module_css1 from './Module_css/Module_css1'
 import Module_css2 from './Module_css/Module_css2'
+import Props_main from './Props/Props_main'
+import App_Route from './Routing/App_Route'
 
 function App() {
   return (
 	<div>
 		{
 			//<Func_component/>
-			//<Class_component/>
+			//<Class_component/>6357496334
 			//<Jsx_comp/>
 
 			//<Layout/>
 
 			//<Css_apply/>
+			//<Module_css1/>
+			
+			//<Props_main/>
 		}
-		<Module_css1/>
+		
+		<App_Route/>
 		
 	</div>
   )
---
+++ React/demo_app/src/Routing/App_Route.jsx
@@ -0,0 +1,39 @@
+/*
+Create React App doesn't include page routing.
+
+React Router is the most popular solution.
+
+Add React Router
+To add React Router in your application, run this in the terminal from the root directory 
+of the application:
+
+npm i -D react-router-dom 
+
+*/
+
+
+import React from 'react'
+import Index from './Pages/Index'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './Pages/About'
+import Blog from './Pages/Blog'
+import Contact from './Pages/Contact'
+import Header from './Component/Header'
+import Footer from './Component/Footer'
+
+function App_Route() {
+  return (
+    <div>
+       <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<><Header/><Index/><Footer/></>}></Route>
+                <Route path="/about" element={<><Header/><About/><Footer/></>}></Route>
+                <Route path="/blog" element={<><Header/><Blog/><Footer/></>}></Route>
+                <Route path="/contact" element={<Contact/>}></Route>
+            </Routes>
+       </BrowserRouter>
+    </div>
+  )
+}
+
+export default App_Route
